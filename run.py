ecd59772cb94abe58488ae341d427dfc70c01d3f
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-06-20 21:46:40 +0000

    Add welcome to message to the board

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def print_welcome_message():
+    """
+    Prints a welcome message to the players.
+    """
+    print("Welcome to Tic Tac Toe!")
+    print("You will be prompted to enter the size of the board.")
+    print("Players will take turns to enter their moves.")
+    print("The first player to align their marks in a row, column, or diagonal wins!")
+    print("Let's get started!\n")
+
