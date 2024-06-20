af276e69c19321e4fd2f7a2269c22e01bda5fa8f
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-06-20 21:58:51 +0000

    Add board for the game

--- run.py
+++ run.py
@@ -8,3 +8,11 @@ def print_welcome_message():
     print("The first player to align their marks in a row, column, or diagonal wins!")
     print("Let's get started!\n")
 
+def print_board(board):
+    """
+    Prints the current state of the Tic Tac Toe board.
+    """
+    n = len(board)
+    for row in board:
+        print(" | ".join(row))
+        print("-" * (n * 2 - 1))
